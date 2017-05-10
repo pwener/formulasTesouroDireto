0fa20c8ba6dafe6bc13587766ab7278934b4d10b
pwener <phwener@gmail.com>
2017-05-10 16:49:23 -0300

    Criacao do BTN
    Signed-off-by: luisfrvilela <luisfrvilela@gmail.com>

---
+++ src/github/fga/das/tdd/BTN.java
@@ -0,0 +1,19 @@
+package github.fga.das.tdd;
+
+public class BTN {
+
+	private Double precoUnitarioAnterior;
+	private Double taxaReferencial;
+	
+	public void setPrecoUnitarioAnterior(Double preco) {
+		this.precoUnitarioAnterior = preco;
+	}
+
+	public void setTaxaReferencial(Double taxa) {
+		this.taxaReferencial = taxa;
+	}
+
+	public Double getPrecoUnitatio() {
+		return 10.0;
+	}
+}
--- test/formulasTesouroDireto/BTNTest.java
+++ test/formulasTesouroDireto/BTNTest.java
@@ -5,19 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import github.fga.das.tdd.BTN;
+
 public class BTNTest {
 	
 	private BTN btn;
 	
 	@Before
 	public void setUp() {
-		this.btn = new BTNTest();
+		this.btn = new BTN();
 	}
 	
 	@Test
 	public void testPrecoUnitario() {
-		btn.setPrecoUnitarioAnterior(5);
-		btn.setTaxaReferencial(2);
-		assertEquals(10, btn.getPrecoUnitatio());
+		btn.setPrecoUnitarioAnterior(5.0);
+		btn.setTaxaReferencial(2.0);
+		assertEquals(10, btn.getPrecoUnitatio(), 0.000006);
 	}
 }
