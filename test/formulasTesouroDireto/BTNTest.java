7a464e246853c34d9567ecb495d32b35967938ad
pwener <phwener@gmail.com>
2017-05-10 16:41:09 -0300

    Criando BTN teste
    Signed-off-by: luisfrvilela <luisfrvilela@gmail.com>

--- test/formulasTesouroDireto/BTNTest.java
+++ test/formulasTesouroDireto/BTNTest.java
@@ -1,5 +1,23 @@
 package formulasTesouroDireto;
 
-public class BTNTest {
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTNTest {
+	
+	private BTN btn;
+	
+	@Before
+	public void setUp() {
+		this.btn = new BTNTest();
+	}
+	
+	@Test
+	public void testPrecoUnitario() {
+		btn.setPrecoUnitarioAnterior(5);
+		btn.setTaxaReferencial(2);
+		assertEquals(10, btn.getPrecoUnitatio());
+	}
 }
