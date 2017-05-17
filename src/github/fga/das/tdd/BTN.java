4b2fb8921972661cc6d986c420abf8ca0fe6b114
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-17 17:34:39 -0300

    Remove old version of FatorJuros property

--- src/github/fga/das/tdd/BTN.java
+++ src/github/fga/das/tdd/BTN.java
@@ -4,9 +4,8 @@ public class BTN {
 
 	private Double precoUnitarioAnterior;
 	private Double taxaReferencial;
-	private Double fatorJuros;
 	private double precoUnitario;
-	private FatorJuros _fatorJuros = new FatorJuros(10.0, 12);
+	private FatorJuros fatorJuros = new FatorJuros(10.0, 12);
 
 	public void setPrecoUnitarioAnterior(Double preco) {
 		this.precoUnitarioAnterior = preco;
@@ -36,12 +35,8 @@ public class BTN {
 		this.precoUnitario = preco;
 	}
 
-	public void setFatorJuros(Double juros) {
-		this.fatorJuros = juros;
-	}
-
 	public Double getFatorJuros() {
-		return this._fatorJuros.calculaFatorJuros();
+		return this.fatorJuros.calculaFatorJuros();
 	}
 
 	public double getJuros() {
