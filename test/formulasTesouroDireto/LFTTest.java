78a669747eb5ef53775fdf6a0fc6a5f1a925cb8b
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-18 17:15:08 -0300

    Adding VNb set get test

--- test/formulasTesouroDireto/LFTTest.java
+++ test/formulasTesouroDireto/LFTTest.java
@@ -10,6 +10,7 @@ import github.fga.das.tdd.LFT;
 public class LFTTest {
 	
 	private LFT lft;
+	private static final double PRECISION6 = 0.000000;
 
 	@Before
 	public void setUp() {
@@ -20,5 +21,12 @@ public class LFTTest {
 	public void testLFTNotNull() {
 		assertNotNull(lft);
 	}
+	
+	@Test
+	public void testeVNbPrecision() {
+	 	lft.setVBn(70.987654321);
+	 	
+	 	assertEquals(lft.getVNb(), 70.987654, PRECISION6);
+	 }
 
 }
