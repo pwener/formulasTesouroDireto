da9e6e53a9bed225307c50e4a3494e6941ea22eb
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-18 17:06:06 -0300

    Add LFT class

---
+++ src/github/fga/das/tdd/LFT.java
@@ -0,0 +1,9 @@
+package github.fga.das.tdd;
+
+public class LFT {
+	
+	public LFT() {
+		
+	}
+
+}
--- test/formulasTesouroDireto/LFTTest.java
+++ test/formulasTesouroDireto/LFTTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import github.fga.das.tdd.LFT;
+
 public class LFTTest {
 	
 	private LFT lft;
