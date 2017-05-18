06e0865cf0a86a609241c8e5b68dcb3cb2da5182
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-18 19:31:33 -0300

    Add truncate method

--- src/github/fga/das/tdd/HandleDecimal.java
+++ src/github/fga/das/tdd/HandleDecimal.java
@@ -1,5 +1,10 @@
 package github.fga.das.tdd;
 
-public class HandleDecimal {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+public class HandleDecimal {
+	public static double truncate(double number, int decimalPlaces) {
+		return BigDecimal.valueOf(number).setScale(decimalPlaces, RoundingMode.FLOOR).doubleValue();
+	}
 }
