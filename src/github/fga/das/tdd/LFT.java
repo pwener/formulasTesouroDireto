18f2d01354102935e4e8bff43e4ff1f65f2311d1
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-18 17:22:43 -0300

    LFT: add VNb attribute to class

--- src/github/fga/das/tdd/LFT.java
+++ src/github/fga/das/tdd/LFT.java
@@ -1,9 +1,22 @@
 package github.fga.das.tdd;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class LFT {
+	
+	private double VNb;
 	
 	public LFT() {
 		
+	}
+	
+	public void setVBn(double number) {
+		this.VNb = BigDecimal.valueOf(number).setScale(6, RoundingMode.FLOOR).doubleValue();
+	}
+	
+	public double getVNb() {
+		return this.VNb;
 	}
 
 }
