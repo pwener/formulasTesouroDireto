b5ed689343e3ea2e812cbdddef9589e7c3da7a4a
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-19 15:23:09 -0300

    LFTA: add implementation of tac

--- src/github/fga/das/tdd/LFTA.java
+++ src/github/fga/das/tdd/LFTA.java
@@ -2,6 +2,10 @@ package github.fga.das.tdd;
 
 public class LFTA {
 	private int N;
+	private int P;
+	private int Du;
+	private int Dut;
+	private static final double tac = 0.0245; 
 	
 	public void setN(int N) {
 		this.N = N;
@@ -9,5 +13,33 @@ public class LFTA {
 	
 	public int getN() {
 		return this.N;
+	}
+	
+	public void setP(int P) {
+		this.P = P;
+	}
+	
+	public int getP() {
+		return this.P;
+	}
+
+	public int getDu() {
+		return Du;
+	}
+
+	public void setDu(int du) {
+		Du = du;
+	}
+
+	public int getDut() {
+		return Dut;
+	}
+
+	public void setDut(int dut) {
+		Dut = dut;
+	}	
+	
+	public double getTac() {
+		return this.tac;
 	}
 }
