181a6bf42b061187d92a0739548185984bfc4077
pwener <phwener@gmail.com>
2017-05-19 14:55:00 -0300

    Implementing FatorRendimento with test failling

---
+++ src/github/fga/das/tdd/FatorRendimento.java
@@ -0,0 +1,31 @@
+package github.fga.das.tdd;
+
+public class FatorRendimento {
+	
+	private Double ipca1;
+	private Double ipca0;
+	private Integer diasCorridos;
+	private Integer diasCorridosEmQuinzena;
+
+	public void setIPCA1(Double ipca1) {
+		this.ipca1 = ipca1;
+	}
+
+	public void setIPCA0(Double ipca0) {
+		this.ipca0 = ipca0;
+	}
+
+	public void setDiasCorridos(Integer dias) {
+		this.diasCorridos = dias;
+	}
+
+	public void setDiasCorridosEmQuinzena(Integer dias) {
+		this.diasCorridosEmQuinzena = dias;
+	}
+
+	public double calcula() {
+		return 0;
+	}
+
+
+}
--- test/formulasTesouroDireto/FatorRendimentoTest.java
+++ test/formulasTesouroDireto/FatorRendimentoTest.java
@@ -1,26 +1,28 @@
 package formulasTesouroDireto;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import github.fga.das.tdd.FatorRendimento;
+
 public class FatorRendimentoTest {
-	
+
 	private static final double ERRO = 0.000001;
 
 	private FatorRendimento fator;
-	
+
 	@Before
 	public void setUp() {
 		this.fator = new FatorRendimento();
 	}
-	
+
 	@Test
 	public void testCalcula() {
 		fator.setIPCA1(0.8);
 		fator.setIPCA0(0.5);
 		fator.setDiasCorridos(20);
-		fator.setDiasCorridosPorQuinzena(5);
-		Assert.assertEquals(6.55360, fator.calcula(), ERRO);
+		fator.setDiasCorridosEmQuinzena(5);
+		assertEquals(6.55360, fator.calcula(), ERRO);
 	}
 }
