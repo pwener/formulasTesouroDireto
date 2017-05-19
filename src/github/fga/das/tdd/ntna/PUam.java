b736954b1a3103e0726980fe041492a34d2b1bb9
luisresende <luisfrvilela@gmail.com>
2017-05-19 18:40:07 -0300

    Create class PUam

---
+++ src/github/fga/das/tdd/ntna/PUam.java
@@ -0,0 +1,25 @@
+package github.fga.das.tdd.ntna;
+
+public class PUam {
+
+	public void setValorDolarA(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setValorDolarB(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setValorLiquido(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double calcula() {
+		// TODO Auto-generated method stub
+		return 45.0;
+	}
+
+}
