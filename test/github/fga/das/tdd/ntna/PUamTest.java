abb09da68a4770a861c48d2e0f6c4805b7a51f79
luisresende <luisfrvilela@gmail.com>
2017-05-19 19:16:34 -0300

    Refactor: PUamTest

--- test/github/fga/das/tdd/ntna/PUamTest.java
+++ test/github/fga/das/tdd/ntna/PUamTest.java
@@ -10,18 +10,21 @@ public class PUamTest {
 	private static final double ERRO = 0.00001;
 
 	private PUam puam;
+	private ValorLiquido valorLiquido;
 	
 	@Before
 	public void setUp() {
 		this.puam = new PUam();
+		this.valorLiquido = new ValorLiquido();
 	}
 	
 	@Test
 	public void testCalcula(){
 		puam.setDolarA(3.2);
 		puam.setDolarB(3.0);
-		puam.setValorEmissao(20.0);
-		puam.setPercentualAmortizacao(0.9);
+		valorLiquido.setValorEmissao(20.0);
+		valorLiquido.setPercentualAmortizacao(0.9);
+		puam.setValorLiquido(valorLiquido);
 		assertEquals(19.2, puam.calcula(), ERRO);
 	}
 
