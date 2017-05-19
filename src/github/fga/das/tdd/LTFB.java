4eeb1c163daf42859d1dced673fb29efe991aae2
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-19 17:07:14 -0300

    LTFB: add selick implementation

--- src/github/fga/das/tdd/LTFB.java
+++ src/github/fga/das/tdd/LTFB.java
@@ -1,5 +1,25 @@
 package github.fga.das.tdd;
 
-public class LTFB {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LTFB {
+	
+	private List<Double> selicK;
+	
+	public LTFB() {
+		selicK = new ArrayList<>();
+	}
+	
+	public void addSelicK(double number) {
+		selicK.add(HandleDecimal.truncate(number, 2));
+	}
+	
+	public List<Double> getSelicK() {
+		return selicK;
+	}
+	
+	public double getSelicK(int i) {
+		return selicK.get(i-1);
+	}
 }
