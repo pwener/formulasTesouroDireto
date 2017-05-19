b1009cb20e3f58b66d7311aa62db07d39c6e13cb
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-19 16:14:52 -0300

    LFTA: add implementation of C property

--- src/github/fga/das/tdd/LFTA.java
+++ src/github/fga/das/tdd/LFTA.java
@@ -5,6 +5,7 @@ public class LFTA {
 	private int P;
 	private int Du;
 	private int Dut;
+	private double selicA;
 	private static final double tac = 0.0245; 
 	
 	public void setN(int N) {
@@ -42,4 +43,28 @@ public class LFTA {
 	public double getTac() {
 		return this.tac;
 	}
+	
+	public void setSelicA(double n) {
+		this.selicA = HandleDecimal.truncate(n, 2);
+	}
+	
+	public double getSelicA() {
+		return this.selicA;
+	}
+	
+	public double getTSelicD() {
+		double tselic = Math.pow((1.0 + this.getSelicA()/100.0), (1.0/252.0)) - 1;
+		return HandleDecimal.round(tselic,8);
+	}
+	
+	public double getC(int n) {
+		double c = 1;
+		for(int i =0; i< n; i++) {
+			c *= (1 + this.getTSelicD());
+		}
+		
+		return HandleDecimal.round(c, 8);
+	}
+	
+	
 }
