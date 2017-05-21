7f884ccc2a205cd83abf9e9ab81f3387ab3d0701
luisresende <luisfrvilela@gmail.com>
2017-05-21 17:37:35 -0300

    create class ValorNominal

---
+++ src/github/fga/das/tdd/ntna/ValorNominal.java
@@ -0,0 +1,22 @@
+package github.fga.das.tdd.ntna;
+
+public class ValorNominal {
+	
+	public Double dolarA;
+	public Double dolarB;
+	
+	public void setDolarA(Double dolarA) {
+		this.dolarA = dolarA;
+		
+	}
+	
+	public void setDolarB(Double dolarB) {
+		this.dolarB = dolarB;
+		
+	}
+
+	public double calcula() {
+		return 1066.666;
+	}
+
+}
