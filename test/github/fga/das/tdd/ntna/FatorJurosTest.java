f5dfdf79053da3196e94b700d5d74540aa5209e1
luisresende <luisfrvilela@gmail.com>
2017-05-21 16:56:20 -0300

    Refactor FatorJurosTest

--- test/github/fga/das/tdd/ntna/FatorJurosTest.java
+++ test/github/fga/das/tdd/ntna/FatorJurosTest.java
@@ -2,26 +2,16 @@ package github.fga.das.tdd.ntna;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class FatorJurosTest {
 	
 	private static final double ERRO = 0.00001;
-
-	private FatorJuros fatorJuros;
-	
-	@Before
-	public void setUp() {
-		this.fatorJuros = new FatorJuros();
-	}
 	
 	@Test
 	public void testCalcula(){
-		fatorJuros.setDtp(210.0);
-		fatorJuros.setDtup(200.0);
-		fatorJuros.setTaxaJuros(10);
-		assertEquals(0,002777777, fatorJuros.calcula(), ERRO);
+		FatorJuros fatorJuros = new FatorJuros(210.0,200.0,10.0);
+		assertEquals(0.00277777, fatorJuros.calcula(), ERRO);
 	}
 
 }
