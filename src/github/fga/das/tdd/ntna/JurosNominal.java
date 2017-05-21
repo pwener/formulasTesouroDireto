7520b74d5b1594c05dfd77572a8041f6d7d7b8ee
luisresende <luisfrvilela@gmail.com>
2017-05-21 18:04:58 -0300

    Refactor class JurosNominal

--- src/github/fga/das/tdd/ntna/JurosNominal.java
+++ src/github/fga/das/tdd/ntna/JurosNominal.java
@@ -3,18 +3,18 @@ package github.fga.das.tdd.ntna;
 public class JurosNominal {
 	
 	public ValorNominal valorNominal;
-	public Double fatorJurosNominal;
+	public FatorJurosNominal fatorJurosNominal;
 	
 	public void setValorNominal(ValorNominal valorNominal) {
 		this.valorNominal = valorNominal;
 	}
 
-	public void setFatorJurosNominal(Double fatorJurosNominal) {
+	public void setFatorJurosNominal(FatorJurosNominal fatorJurosNominal) {
 		this.fatorJurosNominal = fatorJurosNominal;
 	}
 
 	public double calcula() {
-		return this.valorNominal.calcula()*this.fatorJurosNominal;
+		return this.valorNominal.calcula()*this.fatorJurosNominal.calcula();
 	}
 
 }
