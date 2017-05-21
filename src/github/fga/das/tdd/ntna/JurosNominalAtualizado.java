19560465b14d78f42b126f06e9f81c539e9357fc
luisresende <luisfrvilela@gmail.com>
2017-05-21 18:45:36 -0300

    Create class JurosNominalAtualizado

---
+++ src/github/fga/das/tdd/ntna/JurosNominalAtualizado.java
@@ -0,0 +1,21 @@
+package github.fga.das.tdd.ntna;
+
+public class JurosNominalAtualizado {
+	
+	public Double fatorJurosNominalAtualizado;
+	public Double valorNominalAtualizado;
+	
+	public void setFatorJurosNominalAtualizado(Double fatorJurosNominalAtualizado) {
+		this.fatorJurosNominalAtualizado = fatorJurosNominalAtualizado;
+	}
+
+	public void setValorNominalAtualizado(Double valorNominalAtualizado) {
+		this.valorNominalAtualizado = valorNominalAtualizado;
+	}
+
+	public double calcula() {
+		return 533.33333;
+	}
+	
+
+}
