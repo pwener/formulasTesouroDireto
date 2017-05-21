e1918edeaf037eda486f16b535154cd4b0e7e616
luisresende <luisfrvilela@gmail.com>
2017-05-21 17:00:23 -0300

    Refactor Juros

--- src/github/fga/das/tdd/ntna/Juros.java
+++ src/github/fga/das/tdd/ntna/Juros.java
@@ -3,19 +3,19 @@ package github.fga.das.tdd.ntna;
 public class Juros {
 	
 	private PUam puam;
-	private Double fatorJuros;
+	private FatorJuros fatorJuros;
 	
 	public void setPuam(PUam puam) {
 		this.puam = puam;
 	}
 
-	public void setFatorJuros(Double fatorJuros) {
+	public void setFatorJuros(FatorJuros fatorJuros) {
 		this.fatorJuros = fatorJuros;
 	}
 
 	public double calcula() {
 		
-		return this.puam.calcula()*this.fatorJuros;
+		return this.puam.calcula()*this.fatorJuros.calcula();
 	}
 
 }
