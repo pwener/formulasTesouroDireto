256acaa5787a4d8a66a57f3aba4c64696aac0f33
luisresende <luisfrvilela@gmail.com>
2017-05-21 18:02:12 -0300

    Refactor class FatorJurosNominalTest

--- test/github/fga/das/tdd/ntna/FatorJurosNominalTest.java
+++ test/github/fga/das/tdd/ntna/FatorJurosNominalTest.java
@@ -8,18 +8,10 @@ import org.junit.Test;
 public class FatorJurosNominalTest {
 	
 	private static final double ERRO = 0.00001;
-
-	private FatorJurosNominal fatorjurosNominal;
-	
-	@Before
-	public void setUp() {
-		this.fatorjurosNominal = new FatorJurosNominal();
-	}
 	
 	@Test
 	public void testCalcula(){
-		fatorjurosNominal.setTaxaJuros(10.0);
-		fatorjurosNominal.setNumeroMeses(6);
+		FatorJurosNominal fatorjurosNominal = new FatorJurosNominal(10.0,6);
 		assertEquals(0.05, fatorjurosNominal.calcula(), ERRO);
 	}
 
